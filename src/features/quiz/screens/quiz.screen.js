20c7e4d7d99a6a235a1be989fca4c1f0a4b46668
Bertrand <mutanganabertrand@gmail.com>
2023-01-10 11:23:58 +0200

    Add the top bar

--- src/features/quiz/screens/quiz.screen.js
+++ src/features/quiz/screens/quiz.screen.js
@@ -1,15 +1,47 @@
-import { StyleSheet, Text } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Badge } from 'react-native-paper'
 import { SafeArea } from '../../../utils/safe-area.components'
+import questions from '../../../services/quiz/mock/questions'
 
 const QuizScreen = () => {
+  const currentQuestion = questions[0]
   return (
     <SafeArea>
-      <Text>QuizScreen</Text>
+      <View style={styles.flexRow}>
+        <Text style={styles.heading}>Quiz Challenge 1</Text>
+        <Badge size={45} style={styles.badge}>
+          15
+        </Badge>
+      </View>
+      <View style={styles.flexRow}>
+        <Text style={styles.subHeading}>Your Progress</Text>
+        <Text>(0/5) answered</Text>
+      </View>
     </SafeArea>
   )
 }
 
 export default QuizScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  flexRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    marginVertical: 5,
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#666AF6',
+  },
+  subHeading: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  badge: {
+    backgroundColor: '#666AF6',
+  },
+})
