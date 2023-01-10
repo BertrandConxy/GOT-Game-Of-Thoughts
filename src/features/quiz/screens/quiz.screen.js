879c7e7bed8a765c62b73f24ea80d52ef8daa392
Bertrand <mutanganabertrand@gmail.com>
2023-01-10 10:47:33 +0200

    Add safe area

--- src/features/quiz/screens/quiz.screen.js
+++ src/features/quiz/screens/quiz.screen.js
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import React from 'react'
+import { SafeArea } from '../../../utils/safe-area.components'
 
 const QuizScreen = () => {
   return (
-    <View>
+    <SafeArea>
       <Text>QuizScreen</Text>
-    </View>
+    </SafeArea>
   )
 }
 
