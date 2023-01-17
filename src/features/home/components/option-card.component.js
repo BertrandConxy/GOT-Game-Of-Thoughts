5d0f02251308a9fe5c4207f373e9d24f5ebe1a19
Bertrand <mutanganabertrand@gmail.com>
2023-01-17 16:24:54 +0200

    Solve bug

--- src/features/home/components/option-card.component.js
+++ src/features/home/components/option-card.component.js
@@ -3,11 +3,12 @@ import { StyleSheet, ImageBackground, View } from 'react-native'
 import { Card, Text } from 'react-native-paper'
 
 const OptionCard = ({ imageCover, option }) => {
+  const image = require(imageCover)
   return (
     <Card elevation={2} mode="elevated" style={styles.card}>
       <Card.Content style={styles.cardContent}>
         <ImageBackground
-          source={{ uri: { imageCover } }}
+          source={imageCover}
           style={{
             width: 340,
             height: 200,
