0a46dad49bda9f422e7b033fd048c1399b2e5c5c
Bertrand <mutanganabertrand@gmail.com>
2023-01-18 14:12:05 +0200

    Edit image cards

--- src/features/home/components/options.js
+++ src/features/home/components/options.js
@@ -5,14 +5,14 @@ export const options = [
   },
   {
     option: 'B&BG',
-    imageCover: require('../../../../assets/leaderboard-vector-1.jpg'),
+    imageCover: require('../../../../assets/battle-vector-2.jpg'),
   },
   {
-    option: 'LeaderBoard',
-    imageCover: require('../../../../assets/leaderboard-vector-1.jpg'),
+    option: 'Contests',
+    imageCover: require('../../../../assets/contests-vector.jpg'),
   },
   {
-    option: 'LeaderBoard',
-    imageCover: require('../../../../assets/leaderboard-vector-1.jpg'),
+    option: 'Survey',
+    imageCover: require('../../../../assets/survey-vector.jpg'),
   },
 ]
