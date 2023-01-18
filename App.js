142db50c5e7bff5146dfa1b91dc170c69a70652f
Bertrand <mutanganabertrand@gmail.com>
2023-01-18 15:15:39 +0200

    Add theme

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import { StatusBar } from 'expo-status-bar'
-import Navigation from './src/infrastructure/navigation'
 import { StyleSheet, View } from 'react-native'
+import { ThemeProvider } from 'styled-components/native'
+import Navigation from './src/infrastructure/navigation'
+import { theme } from './src/infrastructure/theme'
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald'
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato'
 
 export default function App() {
+  const [OswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  })
+  const [LatoLoaded] = useLato({
+    Lato_400Regular,
+  })
+
+  if (!OswaldLoaded || !LatoLoaded) {
+    return null
+  }
   return (
     <View style={styles.container}>
       <Navigation />
---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,30 @@
+export const colors = {
+  brand: {
+    primary: '#666AF6',
+    secondary: '#5282BD',
+    muted: '#C6DAF7',
+  },
+  ui: {
+    primary: '#262626',
+    secondary: '#757575',
+    tertiary: '#F1F1F1',
+    quaternary: '#FFFFFF',
+    disabled: '#DEDEDE',
+    error: '#D0421B',
+    success: '#138000',
+  },
+  bg: {
+    primary: '#FFFFFF',
+    secondary: '#F1F1F1',
+    tertiary: '#666AF6',
+  },
+  text: {
+    primary: '#262626',
+    secondary: '#757575',
+    tertiary: '#666AF6',
+    disabled: '#9C9C9C',
+    inverse: '#FFFFFF',
+    error: '#D0421B',
+    success: '#138000',
+  },
+}
