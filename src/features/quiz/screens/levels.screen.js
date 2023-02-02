b2e8f3151445da1836586bf880a4c6673eecb32f
Bertrand <mutanganabertrand@gmail.com>
2023-02-02 19:05:42 +0200

    Add

--- src/features/quiz/screens/levels.screen.js
+++ src/features/quiz/screens/levels.screen.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import { TouchableOpacity } from 'react-native'
-import { categories } from '../../../services/quiz/categories/categories'
 import { SafeArea } from '../../../utils/safe-area.components'
 import CategoryCard from '../components/category-card.component'
 import { ScrollContainer } from '../../../components/scroll-view/scroll-view.component'
