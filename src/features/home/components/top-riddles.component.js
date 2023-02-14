bb56a1892db298eecfc9931e829b081ce568ff99
Bertrand <mutanganabertrand@gmail.com>
2023-02-14 17:55:39 +0200

    Create the top riddles component

---
+++ src/features/home/components/top-riddles.component.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+import styled from 'styled-components/native'
+import ProfileCard from './profile-card.component'
+
+const TopRiddles = () => {
+  return (
+    <View>
+      <ProfileCard />
+    </View>
+  )
+}
+
+export default TopRiddles
--- src/features/home/screens/leaderboard.screen.js
+++ src/features/home/screens/leaderboard.screen.js
@@ -4,6 +4,7 @@ import { SafeArea } from '../../../utils/safe-area.components'
 import { Spacer } from '../../../components/spacer/spacer.component'
 import Header from '../components/header.component'
 import TopRated from '../components/top-rated.component'
+import TopRiddles from '../components/top-riddles.component'
 
 const LeaderBoardScreen = () => {
   return (
@@ -13,6 +14,7 @@ const LeaderBoardScreen = () => {
       <Spacer position="bottom" size="large" />
       <Divider />
       <Header title="Best In Riddles" />
+      <TopRiddles />
     </SafeArea>
   )
 }
