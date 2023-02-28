0c3386174f38947b6638359c9ecac1195cde4ba8
Bertrand <mutanganabertrand@gmail.com>
2023-02-28 17:32:34 +0200

    Add new files

--- src/features/home/screens/contests.screen.js
+++ src/features/home/screens/contests.screen.js
@@ -7,23 +7,10 @@ import Header from '../components/header.component'
 import TopRated from '../components/top-rated.component'
 import TopRiddles from '../components/top-riddles.component'
 import TopGk from '../components/top-gk.component'
+import QuestionnaireComponent from '../../../components/questionnaire/questionnaire.component'
 
-const ContestsScreen = () => {
-  return (
-    <ScrollView>
-      <SafeArea>
-        <Header title="GOTs" size="primary" />
-        <TopRated />
-        <Spacer position="bottom" size="large" />
-        <Divider />
-        <Header title="Best In Riddles" />
-        <TopRiddles />
-        <Divider />
-        <Header title="Best In GK" />
-        <TopGk />
-      </SafeArea>
-    </ScrollView>
-  )
+const ContestsScreen = ({ navigation }) => {
+  return <SafeArea></SafeArea>
 }
 
 export default ContestsScreen
