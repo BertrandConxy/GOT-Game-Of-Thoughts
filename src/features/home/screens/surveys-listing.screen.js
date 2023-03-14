bcfdcd21b4420a403cdb963bb77617cea2ce9776
Bertrand <mutanganabertrand@gmail.com>
2023-03-14 16:45:29 +0200

    Add

--- src/features/home/screens/surveys-listing.screen.js
+++ src/features/home/screens/surveys-listing.screen.js
@@ -1,19 +1,23 @@
 import React from 'react'
-
 import { SafeArea } from '../../../utils/safe-area.components'
-
+import { EmptyStorage } from '../../../components/not-found/resource-not-found'
 import ListingComponent from '../../../components/listing/listing.component'
+import surveys from '../../../services/surveys/mock/surveys'
 
-import QuestionnaireComponent from '../../../components/questionnaire/questionnaire.component'
-
-const SurveysListing = ({ navigation }) => {
-  const array = new Array(10).fill({ name: 'Surveys' })
+const ContestsListing = ({ navigation }) => {
   return (
     <SafeArea>
-      <ListingComponent array={array} />
+      {contests.length === 0 ? (
+        <EmptyStorage name="Contests" />
+      ) : (
+        <ListingComponent
+          array={surveys}
+          navigation={navigation}
+          Next="SurveysReady"
+        />
+      )}
     </SafeArea>
   )
 }
 
-export default SurveysListing
-// HANDLE THE CASE WHEN THERE ARE NO CONTESTS
+export default ContestsListing
