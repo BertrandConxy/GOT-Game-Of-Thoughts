71fb8d5bd34d2f71d1695e8792bc98916c1e8da6
Bertrand <mutanganabertrand@gmail.com>
2023-04-25 16:08:51 +0200

    Built the battle screen

--- src/features/battle/screens/getReady.screen.js
+++ src/features/battle/screens/getReady.screen.js
@@ -8,44 +8,65 @@ import { Spacer } from '../../../components/spacer/spacer.component'
 
 const ReadyView = styled.View`
   align-items: center;
+  justify-content: center;
+  flex: 1;
 `
-const RulesContainer = styled.View`
+const Rules = styled.View`
   background-color: ${(props) => props.theme.colors.bg.primary};
   padding: ${(props) => props.theme.space[3]};
   margin: ${(props) => props.theme.space[3]};
   border-radius: ${(props) => props.theme.sizes[0]};
 `
 
+const PlayersContainer = styled.View`
+  align-items: center;
+`
+
+const RulesContainer = styled.View`
+  align-items: center;
+  justify-content: flex-end;
+`
+
+const Players = styled.Text`
+  font-size: 120px;
+`
+
 const GetReady = ({ navigation }) => {
   return (
     <ReadyView>
+      <PlayersContainer>
+        <Text variant="titleBrand">Number of Participants</Text>
+        <Players>5</Players>
+      </PlayersContainer>
       <Spacer position="top" size="medium" />
-      <Text variant="titleBrand">Quiz Rules</Text>
-      <Spacer position="bottom" size="small" />
       <RulesContainer>
-        <Text variant="bodyWhite">
-          {' '}
-          * For each correct answer you get 5 points{' '}
-        </Text>
-        <Text variant="bodyWhite">
-          {' '}
-          * There is no negative marking for the wrong answer{' '}
-        </Text>
-        <Text variant="bodyWhite">
-          {' '}
-          * Each question has a time of 15 seconds{' '}
-        </Text>
-        <Text variant="bodyWhite"> * All questions are compulsory </Text>
+        <Text variant="titleBrand">Battle Rules</Text>
+        <Spacer position="bottom" size="small" />
+        <Rules>
+          <Text variant="bodyWhite">
+            {' '}
+            * The Battle is 2 minutes long so, use your time efficiently!{' '}
+          </Text>
+          <Text variant="bodyWhite">
+            {' '}
+            * For each correct answer you get 5 points{' '}
+          </Text>
+          <Text variant="bodyWhite">
+            {' '}
+            * There is no negative marking for the wrong answer{' '}
+          </Text>
+          <Text variant="bodyWhite"> * All questions are compulsory </Text>
+        </Rules>
+        <TouchableOpacity onPress={() => navigation.navigate('BattleScreen')}>
+          <Button
+            icon="flag-checkered"
+            mode="contained"
+            buttonColor={theme.colors.bg.primary}
+          >
+            Start Quiz
+          </Button>
+        </TouchableOpacity>
       </RulesContainer>
-      <TouchableOpacity onPress={() => navigation.navigate('QuizRunning')}>
-        <Button
-          icon="flag-checkered"
-          mode="contained"
-          buttonColor={theme.colors.bg.primary}
-        >
-          Start Quiz
-        </Button>
-      </TouchableOpacity>
     </ReadyView>
   )
 }
