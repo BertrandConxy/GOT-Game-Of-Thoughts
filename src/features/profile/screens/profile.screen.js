01f5bec44c8c309d3829f75c1c7f484e43c53ad0
Bertrand Mutangana <bertrand@CHOICEs-MacBook-Pro-2.local>
2024-01-06 12:47:49 +0200

    made profile pic layout

--- src/features/profile/screens/profile.screen.js
+++ src/features/profile/screens/profile.screen.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { ScrollView } from 'react-native'
 import { Text } from 'react-native-paper'
+import { StyleSheet, View, ScrollView } from 'react-native'
 import { SafeArea } from '../../../utils/safe-area.components'
 
 
@@ -8,10 +8,45 @@ const ProfileScreen = () => {
   return (
     <ScrollView>
       <SafeArea>
-      <Text>OK</Text>
+      <View style={styles.card}>
+        <View style={styles.imageHolder}>
+        </View>
+      </View>
+      <View style={styles.summaryContainer}>
+        <View>
+          <Text>255K</Text>
+        </View>
+        <View>
+          <Text>255K</Text>
+        </View>
+        <View>
+          <Text>255K</Text>
+        </View>
+      </View>
       </SafeArea>
     </ScrollView>
   )
 }
 
 export default ProfileScreen
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'red',
+    height: 200,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  imageHolder: {
+    backgroundColor: 'blue',
+    height:120,
+    width:120,
+    alignSelf: 'flex-end',
+  },
+  summaryContainer: {
+    marginVertical: 40,
+    paddingHorizontal: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
+})
