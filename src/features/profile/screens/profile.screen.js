9a08cf20c775208ec8119bc73aec827cc0f66576
Bertrand Mutangana <bertrand@CHOICEs-MacBook-Pro-2.local>
2024-01-06 14:13:40 +0200

    level reached cards

--- src/features/profile/screens/profile.screen.js
+++ src/features/profile/screens/profile.screen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Text } from 'react-native-paper'
 import { StyleSheet, View, ScrollView } from 'react-native'
 import { SafeArea } from '../../../utils/safe-area.components'
+import Header from '../../../components/header/header.component'
 
 
 const ProfileScreen = () => {
@@ -12,16 +13,40 @@ const ProfileScreen = () => {
         <View style={styles.imageHolder}>
         </View>
       </View>
+      <View style={styles.namesContainer}>
+        <View>
+          <Text>Bertrand</Text>
+          <Text>@Conxy</Text>
+        </View>
+      </View>
       <View style={styles.summaryContainer}>
-        <View>
-          <Text>255K</Text>
+        <View style={styles.summary}>
+          <Text>255</Text>
+          <Text>Highest Score</Text>
         </View>
-        <View>
-          <Text>255K</Text>
+        <View style={styles.summary}>
+          <Text>2</Text>
+          <Text>Position</Text>
         </View>
-        <View>
-          <Text>255K</Text>
+        <View style={styles.summary}>
+          <Text>20</Text>
+          <Text>Battles</Text>
         </View>
+      </View>
+
+      <View>
+        <Header title="Badges" />
+        <Text>No badge received yet</Text>
+      </View>
+
+      <Header title="Highest level reached" />
+
+      <View style={styles.levelCardContainer}>
+        <View style={styles.levelCard}>
+        </View>
+        <View style={styles.levelCard}>
+        </View>
+
       </View>
       </SafeArea>
     </ScrollView>
@@ -44,9 +69,30 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-end',
   },
   summaryContainer: {
-    marginVertical: 40,
+    marginVertical: 20,
     paddingHorizontal: 40,
     flexDirection: 'row',
     justifyContent: 'space-around'
+  },
+  summary: {
+    alignItems: 'center'
+  },
+  namesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingVertical: 10,
+  },
+  levelCardContainer: {
+   flexDirection: 'row',
+   justifyContent: 'space-around',
+  },
+  levelCard: {
+    backgroundColor: 'blue',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 2,
+    height: 150,
+    width: 150,
   }
 })
